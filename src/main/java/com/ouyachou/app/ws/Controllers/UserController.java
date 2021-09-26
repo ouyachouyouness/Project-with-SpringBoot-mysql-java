329bafec480b2ae016af4750522e28d6d51e8219
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-09-26 13:07:39 +0200

    Add repositories and User Controller

--- src/main/java/com/ouyachou/app/ws/Controllers/UserController.java
+++ src/main/java/com/ouyachou/app/ws/Controllers/UserController.java
@@ -10,48 +10,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ouyachou.app.ws.request.UserRequest;
-import com.ouyachou.app.ws.responses.UserResponse;
-import com.ouyachou.app.ws.services.UserService;
-import com.ouyachou.app.ws.shared.dto.UserDto;
 
 @RestController
-@RequestMapping("/users")//localhost:8080/users
-
+@RequestMapping("/users") // localhost:8080/users
 public class UserController {
-	
-	@Autowired
-	UserService userService;
-	
+
+
 	@GetMapping
 	public String getUser() {
-		
-		
-		
+
 		return "get user was called";
 	}
-	
+
 	@PostMapping
-	public UserResponse createUser(@RequestBody UserRequest userRequest) {
-		
-		UserDto userDto = new UserDto();
-		
-		BeanUtils.copyProperties(userRequest, userDto);
-		
-		UserDto createUser = userService.createUser(userDto);
-		
-		UserResponse userResponse = new UserResponse();
-		
-		BeanUtils.copyProperties(createUser, userResponse);
-		
-		return userResponse;
+	public String createUser() {
+
+		return " user was created";
+
 	}
-	
+
 	@PutMapping
 	public String updateUser() {
 		return "update user was called";
 	}
-	
+
 	@DeleteMapping
 	public String deleteUser() {
 		return "delete user was called";
--- src/main/java/com/ouyachou/app/ws/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.ouyachou.app.ws.services;
-
-import com.ouyachou.app.ws.shared.dto.UserDto;
-
-public interface UserService {
-	
-	public UserDto createUser(UserDto userDto);
-}
