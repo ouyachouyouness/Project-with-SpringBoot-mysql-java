b0d4858ff610ce0a46613e85e784e5200483a967
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-09-27 21:39:49 +0200

    Spring Security

---
+++ src/main/java/com/ouyachou/app/ws/request/UserLoginRequest.java
@@ -0,0 +1,24 @@
+package com.ouyachou.app.ws.request;
+
+public class UserLoginRequest {
+
+    private String email;
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
