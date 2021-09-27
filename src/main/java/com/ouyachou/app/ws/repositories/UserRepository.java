ebc68726ca7078113cdd7198884886f1388de100
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-09-27 11:11:50 +0200

    feature UserRepository

--- src/main/java/com/ouyachou/app/ws/repositories/UserRepository.java
+++ src/main/java/com/ouyachou/app/ws/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.ouyachou.app.ws.repositories;
 
 import com.ouyachou.app.ws.Entites.UserEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
 }
--- src/main/java/com/ouyachou/app/ws/services/impl/UserServiceImpl.java
+++ src/main/java/com/ouyachou/app/ws/services/impl/UserServiceImpl.java
@@ -1,14 +1,33 @@
 package com.ouyachou.app.ws.services.impl;
 
 
+import com.ouyachou.app.ws.Entites.UserEntity;
+import com.ouyachou.app.ws.repositories.UserRepository;
 import com.ouyachou.app.ws.services.UserService;
 import com.ouyachou.app.ws.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
 
-    public UserDto createUser(UserDto userDto){
+    public UserDto createUser(UserDto user){
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("test paswword");
+        userEntity.setUserId("user test id");
+
+        UserEntity newUser = userRepository.save(userEntity);
+
+        UserDto userDto = new UserDto();
+
+        BeanUtils.copyProperties(newUser,userDto);
+
         return null;
     }
 }
