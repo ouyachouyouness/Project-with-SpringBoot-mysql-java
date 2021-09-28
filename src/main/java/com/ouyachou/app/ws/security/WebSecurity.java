7e2cc2e92d0a0a6ee1bec9326cbcbdf10fcb39b4
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-09-28 10:29:24 +0200

    Try the user connection

--- src/main/java/com/ouyachou/app/ws/security/WebSecurity.java
+++ src/main/java/com/ouyachou/app/ws/security/WebSecurity.java
@@ -29,7 +29,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
                 .permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new AuthenticationFilter(authenticationManager()));
+
 
     }
 
