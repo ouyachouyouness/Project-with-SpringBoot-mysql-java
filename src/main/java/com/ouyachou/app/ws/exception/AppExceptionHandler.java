ba439b989d4fd84e63ae39919bac976bbc597fa3
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-10-01 11:13:36 +0200

    Handling a Specific Exception

---
+++ src/main/java/com/ouyachou/app/ws/exception/AppExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.ouyachou.app.ws.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class AppExceptionHandler {
+
+    @ExceptionHandler(value = {UserException.class})
+    public ResponseEntity<Object> HandleUserException(UserException ex, WebRequest request )
+    {
+
+        return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}
