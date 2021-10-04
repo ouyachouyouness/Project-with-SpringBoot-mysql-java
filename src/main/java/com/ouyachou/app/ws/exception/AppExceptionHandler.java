7f79dc6f5b9e5355f1adb29def61d095b51a9325
OUYACHOU Younes <ouyachouyouness@gmail.com>
2021-10-04 08:58:29 +0200

    More Validation Constraints

--- src/main/java/com/ouyachou/app/ws/exception/AppExceptionHandler.java
+++ src/main/java/com/ouyachou/app/ws/exception/AppExceptionHandler.java
@@ -4,11 +4,14 @@ import com.ouyachou.app.ws.responses.ErrorMessage;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class AppExceptionHandler {
@@ -26,6 +29,18 @@ public class AppExceptionHandler {
         ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
 
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> HandleMethodArgumentNotValid(MethodArgumentNotValidException ex, WebRequest request )
+    {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+
+        return new ResponseEntity<>(errors, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 
 
